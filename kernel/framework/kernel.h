92801965135ada1b79241ce12653636b248c46b0
wysjixiang <wysjixiang@163.com>
2023-12-07 02:07:13 +0800

    add a note in kernel.h for extern keyword

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -9,6 +9,21 @@
   extern mod_##mod##_t __##mod##_obj; \
   mod_##mod##_t *mod = &__##mod##_obj; \
   mod_##mod##_t __##mod##_obj
+// there is a useful trick here! we first use extern to declare mod and then to define it; think how it works...
+// .eg:
+/*
+
+  extern int num;
+  int mul(int a){
+    return a*num;
+    // num is defined after this line but it works! since we use extern to declare before
+  }
+  int num = 10;
+
+*/
+
+
+
 
 MODULE(os) {
   void (*init)();
